355c361996f4c2e5c86e2a6a5fa297819afa6194
Yeicot JJ Sarmiento Polo <yeicotsarmientopolo2@gmail.com>
2024-04-20 12:05:16 -0500

    login first attemp
    visual login UI

---
+++ app/src/main/java/com/example/moodyapp/Login.kt
@@ -0,0 +1,167 @@
+package com.example.moodyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.Visibility
+import com.example.moodyapp.ui.theme.MoodyAppTheme
+
+class Login : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoodyAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(
+            space = 40.dp,
+            alignment = Alignment.CenterVertically,
+        )
+    ) {
+        Row {
+            Column {
+                Text(
+                    text = stringResource(R.string.greetings), fontSize = 30.sp
+                )
+                Text(
+                    text = stringResource(R.string.title_activity_login),
+                    fontSize = 20.sp,
+                )
+            }
+        }
+        Row {
+            Column {
+                var user by remember {
+                    mutableStateOf("")
+                }
+                var pass by remember {
+                    mutableStateOf("")
+                }
+                OutlinedTextField(
+                    value = user,
+                    onValueChange = {
+                        user = it
+                    },
+                    textStyle = LocalTextStyle.current.copy(
+                        textAlign = TextAlign.Left,
+                    ),
+                    label = {
+                        Text(text = stringResource(R.string.user))
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Person, contentDescription = "User"
+                        )
+                    },
+                )
+                var passwordVisible by rememberSaveable { mutableStateOf(false) }
+                OutlinedTextField(value = pass,
+                    onValueChange = {
+                        pass = it
+                    },
+                    textStyle = LocalTextStyle.current.copy(
+                        textAlign = TextAlign.Left,
+                    ),
+                    label = {
+                        Text(text = stringResource(R.string.pass))
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Lock, contentDescription = "Password"
+                        )
+                    },
+                    visualTransformation = if (passwordVisible) VisualTransformation.None
+                    else PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    trailingIcon = {
+                        val image = if (passwordVisible) {
+                            Icons.Filled.Visibility
+                        } else {
+                            Icons.Filled.VisibilityOff
+                        }
+
+                        // Please provide localized description for accessibility services
+                        val description = if (passwordVisible) "Hide password" else "Show password"
+
+                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                            Icon(imageVector = image, description)
+                        }
+                    })
+            }
+        }
+        Row {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .width(OutlinedTextFieldDefaults.MinWidth)
+                    .height(46.dp)
+                    .padding(0.dp)
+            ) {
+                Text(
+                    text = "Ingresar",
+                    fontSize = 18.sp,
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    MoodyAppTheme {
+        LoginScreen()
+    }
+}
