94c8788f04ceedb9583e5e6dc3a5c2e7c01a8bcf
Minorey <sebastestudia@gmail.com>
2024-05-13 12:42:55 -0500

    Onboarding Screen commit

--- app/src/main/java/com/example/moodyapp/presentation/common/TextFields.kt
+++ app/src/main/java/com/example/moodyapp/presentation/common/TextFields.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.Visibility
 import com.example.moodyapp.ui.theme.Quicksand
 
 @OptIn(ExperimentalMaterial3Api::class)
