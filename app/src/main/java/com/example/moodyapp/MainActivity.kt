835d6b6bc390bf851d2fcdaed8dd968398dea28a
Yeicot JJ Sarmiento Polo <yeicotsarmientopolo2@gmail.com>
2024-05-13 11:53:19 -0500

    Login Incorporated
    incorporated login for navegator in app

--- app/src/main/java/com/example/moodyapp/MainActivity.kt
+++ app/src/main/java/com/example/moodyapp/MainActivity.kt
@@ -1,36 +1,22 @@
 package com.example.moodyapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
-import com.example.moodyapp.domain.usecases.app_entry.AppEntryUseCases
 import com.example.moodyapp.presentation.nvgraph.NavGraph
-import com.example.moodyapp.presentation.onboarding.OnBoardingScreen
-import com.example.moodyapp.presentation.onboarding.OnBoardingViewModel
 import com.example.moodyapp.ui.theme.MoodyAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    val viewModel by viewModels<MainViewModel> ()
+    private val viewModel by viewModels<MainViewModel> ()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
---
+++ app/src/main/java/com/example/moodyapp/presentation/common/Buttons.kt
@@ -0,0 +1,126 @@
+package com.example.moodyapp.presentation.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.moodyapp.ui.theme.WhiteSmoke
+
+@Composable
+fun MoodyButton(
+text: String,
+onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = Color.White
+        ),
+        shape = RoundedCornerShape(size = 6.dp)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            color = Color.White
+        )
+    }
+}
+
+@Composable
+fun MoodyTextButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    TextButton(onClick = onClick) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            color = WhiteSmoke
+        )
+    }
+}
+
+@Composable
+fun LoginButton(
+    text: String,
+    onClick: () -> Unit,
+    contentColorValue: Color = Color.White,
+    containerColorValue: Color = MaterialTheme.colorScheme.primary
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .width(OutlinedTextFieldDefaults.MinWidth)
+            .height(46.dp)
+            .padding(0.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = containerColorValue,
+            contentColor = contentColorValue,
+        )
+    ) {
+        Text(
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            text = text,
+            fontSize = 18.sp,
+            color = contentColorValue,
+        )
+    }
+}
+
+@Composable
+fun GoogleButton(
+    text: String,
+) {
+    Button(
+        onClick = {
+        },
+        modifier = Modifier
+            .padding(0.dp, 10.dp, 0.dp, 0.dp)
+            .width(OutlinedTextFieldDefaults.MinWidth)
+            .height(46.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = Color.Black,
+        )
+    ) {
+       Image(painter = painterResource(id = com.google.android.gms.base.R.drawable.googleg_standard_color_18) , contentDescription = "GoogleLogo")
+        Text(
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            text = text,
+            modifier = Modifier.padding(0.dp)
+        )
+    }
+}
+@Composable
+fun LinkButton(
+    text: String,
+    onClick: () -> Unit,
+    contentColorValue: Color = MaterialTheme.colorScheme.background,
+) {
+    TextButton(
+        onClick = onClick,
+        Modifier.padding(0.dp)
+    ) {
+        Text(
+            style = MaterialTheme.typography.labelSmall.copy(fontWeight = FontWeight.Bold),
+            text = text,
+            color = contentColorValue,
+        )
+    }
+}
+
