bb9a3289a979497301348b034850450679f31d5b
Yeicot JJ Sarmiento Polo <yeicotsarmientopolo2@gmail.com>
2024-06-28 23:41:05 -0500

    traducción de menú
    añadir pequeñas traducciones

--- app/src/main/java/com/example/moodyapp/presentation/menu/Screens.kt
+++ app/src/main/java/com/example/moodyapp/presentation/menu/Screens.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.MenuBook
 import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.LibraryBooks
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
@@ -13,6 +14,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import com.example.moodyapp.R
 
 data class Screens(
     val screen: String = "",
@@ -20,20 +23,22 @@ data class Screens(
     val icon: ImageVector = Icons.Filled.Error,
 ) {
     //function to get the list of bottomNavigationItems
+    //el orden de la lista es importante. de arriba hacia abajo == de izquierda a derecha
+    @Composable
     fun bottomNavigationItems(): List<Screens> {
         return listOf(
             Screens(
-                name = "Diary",
+                name = stringResource(id = R.string.memoriesName),
+                icon = Icons.Filled.LibraryBooks,
+                screen = Routes.Calendar.route
+            ),
+            Screens(
+                name = stringResource(id = R.string.diaryName),
                 icon = Icons.AutoMirrored.Filled.MenuBook,
                 screen = Routes.Diary.route,
             ),
             Screens(
-                name = "Calendar",
-                icon = Icons.Filled.CalendarMonth,
-                screen = Routes.Calendar.route
-            ),
-            Screens(
-                name = "Profile",
+                name = stringResource(id = R.string.profileName),
                 icon = Icons.Filled.Person,
                 screen = Routes.Profile.route
             ),
